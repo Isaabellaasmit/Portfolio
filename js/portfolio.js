9fc7064caec123e6a2a25742edb119762ca5246d
Isaabellaasmit <94471118+Isaabellaasmit@users.noreply.github.com>
2022-03-08 16:31:12 +0100

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,20 @@
+
+var mainMenu =
+
+document.querySelector("header #menu");
+
+var menuButton =
+
+document.querySelector("header button");
+
+
+
+menuButton.addEventListener("click", toggleMenu);
+
+
+
+function toggleMenu() {
+
+mainMenu.classList.toggle("toggle");
+
+}
